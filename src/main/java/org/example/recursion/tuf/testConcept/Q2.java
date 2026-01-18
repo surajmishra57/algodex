797dc6c9616a4ebaae131f27826f929d7f674011
suraj mishra <surajmishra574619@gmail.com>
2026-01-18 20:02:04 +0530

    all subsequenc added

--- src/main/java/org/example/recursion/tuf/testConcept/Q2.java
+++ src/main/java/org/example/recursion/tuf/testConcept/Q2.java
@@ -1,15 +1,29 @@
 package org.example.recursion.tuf.testConcept;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Q2 {
     public static void main(String[] args) {
-        int[] arr = new int[]{1, 2, 3, 4, 5, 6, 7};
+        int[] arr = new int[]{1, 2, 3};
         int target = 7;
 
         findAllSubSeqWithEqualSum(arr, target, 0, 0, new ArrayList<Integer>());
         int count = countAllSubSeqWithEqualSum(arr, target, 0, 0);
         System.out.println("count of subseq with sum " + count);
+        System.out.println("print all subsequence");
+        printAllSubSequenceOfArray(arr, 0, new ArrayList<String>());
+    }
+
+    private static void printAllSubSequenceOfArray(int[] arr, int i, ArrayList<String> strings) {
+        if (arr.length <= i) {
+            System.out.println(strings);
+            return;
+        }
+        strings.add(arr[i] + "");
+        printAllSubSequenceOfArray(arr, i + 1, strings);
+        strings.remove(strings.size() - 1);
+        printAllSubSequenceOfArray(arr, i + 1, strings);
     }
 
     private static int countAllSubSeqWithEqualSum(int[] arr, int target, int index, int currSum) {
